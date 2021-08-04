3945733b16228172989b5ad60d394b3496b183e1
Pathi-rao <lakshmipathi0000@gmail.com>
2021-08-04 21:49:14 +0200

    Update test and train

--- Emotion_classifier/test.py
+++ Emotion_classifier/test.py
@@ -16,7 +16,7 @@ train_loader, test_loader = dh.pre_processor(
 # print(test_loader)
 
 classes = ('Angry', 'Disgust', 'Fear', 'Happy',
-           'Neutral', 'Sad', 'Surprise')
+            'Neutral', 'Sad', 'Surprise')
 
 def imshow(img):
     img = img / 2 + 0.5     # unnormalize
@@ -35,6 +35,13 @@ imshow(torchvision.utils.make_grid(images))
 print(' '.join('%5s' % classes[labels[j]] for j in range(batch_size))) #2 is the batch_size
 
 
+"""
+    If you run  the script till this point, it will return 3 outputs. 
+    1. displays the images
+    2. Torch size such as ([2 ,1 ,48, 48)] where 2 is the batch size of images that are being loaded,
+        1 is the grayscale images and 48*48 is the image size.
+    3. Classes of the images
+    """
 
 # alternative way to test the dataloader and display images
 # def imshow(image, ax=None, title=None, normalize=False):
--- Emotion_classifier/train.py
+++ Emotion_classifier/train.py
@@ -14,12 +14,12 @@ from PIL import Image
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print(f"Current device: ", device)
 
-train_x, train_y = dh.create_batch("./datasets/train_dir/",5,512)
-print(train_x.shape)
+# train_x, train_y = dh.create_batch("./datasets/train_dir/",5,512)
+# print(train_x.shape)
 
-val_x, val_y = dh.create_batch("./datasets/test_dir/",5,512)
-print(val_x.shape)
+# val_x, val_y = dh.create_batch("./datasets/test_dir/",5,512)
+# print(val_x.shape)
 
 
-train_losses = []
-val_losses = []
+# train_losses = []
+# val_losses = []
